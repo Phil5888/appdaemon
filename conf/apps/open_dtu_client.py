ef7c07329069aacb9cad51374f71d854e522c14a
Phil <spinner.philipp@gmail.com>
2023-12-31 18:02:05 +0100

    fixed intendation

--- conf/apps/open_dtu_client.py
+++ conf/apps/open_dtu_client.py
@@ -25,8 +25,8 @@ class OpenDtuClientApp(Mqtt):
                 influx_data = []
                 influx_data.append({"measurement": topic_parts[2], "tags": {}, "fields": {topic_parts[3]: data.get("payload")}})
 
-            influx_write_event = InfluxWriteEvent("home", "solar_test", json.dumps(influx_data))
-            self.mqtt_publish("influx/write", influx_write_event.to_json(), namespace="mqtt")
+                influx_write_event = InfluxWriteEvent("home", "solar_test", json.dumps(influx_data))
+                self.mqtt_publish("influx/write", influx_write_event.to_json(), namespace="mqtt")
 
         except Exception as e:
             self.log("OpenDtuClientApp :: MQTT message :: Error in MQTT message received: {}".format(e), level="ERROR")
