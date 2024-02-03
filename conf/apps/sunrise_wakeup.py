bc5407b5368644abbd27be14ac7b5b8ea8ad4d68
Phil <spinner.philipp@gmail.com>
2024-02-03 14:42:09 +0100

    added turn on for media players on init state

--- conf/apps/sunrise_wakeup.py
+++ conf/apps/sunrise_wakeup.py
@@ -184,6 +184,11 @@ class SunriseWakeupApp(Hass):
         for media_player in start_wakeup_event.media_players:
             if media_player not in self.ready_devices:
                 self.call_service(
+                    "media_player/turn_on",
+                    entity_id=media_player,
+                    volume_level=self.wakeup_config.media_players_volume_initial,
+                )
+                self.call_service(
                     "media_player/volume_set",
                     entity_id=media_player,
                     volume_level=self.wakeup_config.media_players_volume_initial,
